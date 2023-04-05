f4a579c0a024a5d4687db68b20252255a53e6403
kikenriq <108308253+kikenriq@users.noreply.github.com>
2023-04-05 12:48:14 -0400

    Added home

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Data = () =>{
+    return(
+        <div className="home__data">
+            <h1 className="home__title">
+                Enrique Farreras
+            </h1>
+            <h3 className="home__subtitle">
+                Frontend Developer
+            </h3>
+            <p className="home__description">I have knowledge in React, Tailwind, JavaScript. I am passionate about my work and always attentive to detail.</p>
+
+            <a href="#contact" className="button button---flex">
+                Contact Me
+            </a>
+        </div>
+    )
+}
+
+export default Data;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="" className="home__social-icon" target="_blank">
+                <i class="ri-github-fill"></i> 
+            </a>
+
+            <a href="" className="home__social-icon" target="_blank">
+                <i class="ri-linkedin-box-fill"></i>
+            </a>
+
+            <a href="" className="home__social-icon" target="_blank">
+                <i class="ri-behance-fill"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social;
