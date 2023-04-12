96696fafa30a08058b85ec8b6826b19882ed51a1
kikenriq <108308253+kikenriq@users.noreply.github.com>
2023-04-12 13:03:39 -0400

    Added Skills section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Skills from './components/skills/Skills';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <main className='main'>
       <Home />
       <About />
+      <Skills />
     </main>
     </>
   );
