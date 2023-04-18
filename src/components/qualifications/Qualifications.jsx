9505e6665167af01156fe2dda20135f111c0c8b8
kikenriq <108308253+kikenriq@users.noreply.github.com>
2023-04-18 13:49:52 -0400

    Qualification added

--- src/components/qualifications/Qualifications.jsx
+++ src/components/qualifications/Qualifications.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import './qualification.css';
 
 
 const Qualifications = () => {
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    };
+
     return(
         <section className="qualification section">
             <h2 className="section__title">Qualification</h2>
@@ -10,17 +16,22 @@ const Qualifications = () => {
 
             <div className="qualification__container container">
                 <div className="qualification__tabs">
-                    <div className="qualification__button qualification__active button--flex">
-                        Education
+                    <div className={toggleState === 1 
+                    ? "qualification__button qualification__active button--flex" 
+                    : "qualification__button button--flex"}>
+
+
+                        
+                        <i class="ri-graduation-cap-line"></i> Education
                     </div>
 
                     <div className="qualification__button button--flex">
-                        Certifications
+                        <i class="ri-award-line"></i> Certifications
                     </div>
                 </div>
 
                 <div className="qualification__sections">
-                    <div className="qualification__content">
+                    <div className="qualification__content-active">
                         <div className="qualification__data">
 
                             <div>
@@ -40,6 +51,10 @@ const Qualifications = () => {
 
                         </div>
 
+                       
+                    </div>
+
+                    <div className="qualification__content">
                         <div className="qualification__data">
                             <div></div>
 
@@ -49,12 +64,50 @@ const Qualifications = () => {
                             </div>
 
                             <div>
-                                <h3 className="qualification__title"></h3>
+                                <h3 className="qualification__title">Google IT Support Professional Certificate</h3>
                                 <span className="qualification__subtitle">
-                                    Universidad De Oriente - Venezuela
+                                    Google/Coursera
                                 </span>
                                 <div className="qualification__calendar">
-                                    <i class="ri-calendar-2-line"></i> 2008 - 2015
+                                    <i class="ri-calendar-2-line"></i> 2022
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+
+                            <div>
+                                <h3 className="qualification__title">UX/UI Design + Figma</h3>
+                                <span className="qualification__subtitle">
+                                    Udemy
+                                </span>
+                                <div className="qualification__calendar">
+                                    <i class="ri-calendar-2-line"></i> 2022
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                        </div>
+
+                        <div className="qualification__data">
+                            <div></div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div>
+                                <h3 className="qualification__title">Meta Frontend Developer</h3>
+                                <span className="qualification__subtitle">
+                                    Meta/Coursera
+                                </span>
+                                <div className="qualification__calendar">
+                                    <i class="ri-calendar-2-line"></i> 2023
                                 </div>
                             </div>
                         </div>
