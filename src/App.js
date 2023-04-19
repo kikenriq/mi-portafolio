9fae483e986dd727380939428cc709879ac854af
kikenriq <108308253+kikenriq@users.noreply.github.com>
2023-04-19 12:37:53 -0400

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Qualifications from './components/qualifications/Qualifications';
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Skills />
       <Qualifications />
+      <Projects />
     </main>
     </>
   );
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import './projects.css'
+import IMG1 from '../../assets/porfolio1.png'
+import IMG2 from '../../assets/porfolio2.png'
+
+const Projects = () => {
+    return(
+        <section className="project section" id="Projects">
+            <h2 className="section__title">Projects</h2>
+            <span className="section__subtitle">My technical level</span>
+
+            <div className="container porfolio__container">
+
+                <article className="porfolio__item">
+                    <div className="porfolio__item-image">
+                        <img src={IMG1} alt="" />
+                    </div>
+                    <h3>Solar-Power Landing page Website.</h3>
+                    <div className="porfolio__item-cta">
+                        <a href="" className="btn" taget='_blank'>Github</a>
+                        <a href="https://kikenriq.github.io/SolarEnergy-Website/" className="btn btn-primary" taget='_blank'>Live Demo</a>
+                    </div>
+                </article>
+
+                <article className="porfolio__item">
+                    <div className="porfolio__item-image">
+                        <img src={IMG2} alt="" />
+                    </div>
+                    <h3>Title Project</h3>
+                    <div className="porfolio__item-cta">
+                        <a href="" className="btn" taget='_blank'>Github</a>
+                        <a href="" className="btn btn-primary" taget='_blank'>Live Demo</a>
+                    </div>
+                </article>
+
+                <article className="porfolio__item">
+                    <div className="porfolio__item-image">
+                        <img src={IMG1} alt="" />
+                    </div>
+                    <h3>Title Project</h3>
+                    <div className="porfolio__item-cta">
+                        <a href="" className="btn" taget='_blank'>Github</a>
+                        <a href="" className="btn btn-primary" taget='_blank'>Live Demo</a>
+                    </div>
+                </article>
+
+            </div>
+        </section>
+    )
+}
+
+export default Projects
