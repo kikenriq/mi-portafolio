464cf421732e4ad617e0dcefb0287513ea05382a
kikenriq <108308253+kikenriq@users.noreply.github.com>
2023-05-03 11:59:29 -0400

    Fixed links and cards

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import './projects.css'
 import IMG1 from '../../assets/porfolio1.png'
 import IMG2 from '../../assets/porfolio2.png'
+import IMG3 from '../../assets/futras.png'
 
 const Projects = () => {
     return(
-        <section className="project section" id="Projects">
+        <section className="project section" id="project">
             <h2 className="section__title">Projects</h2>
             <span className="section__subtitle">My technical level</span>
 
@@ -25,9 +26,19 @@ const Projects = () => {
                     <div className="porfolio__item-image">
                         <img src={IMG2} alt="" />
                     </div>
-                    <h3>Title Project</h3>
+                    <h3>Skincare Product Landing Page</h3>
                     <div className="porfolio__item-cta">
-                        <a href="" className="btn btn-primary" taget='_blank'>Live Demo</a>
+                        <a href="https://kikenriq.github.io/Skin-website/" className="btn btn-primary" taget='_blank'>Live Demo</a>
+                    </div>
+                </article>
+
+                <article className="porfolio__item">
+                    <div className="porfolio__item-image">
+                        <img src={IMG3} alt="" />
+                    </div>
+                    <h3>Futras Fitness & Nutrition</h3>
+                    <div className="porfolio__item-cta">
+                        <a href="https://comfy-douhua-04b6e3.netlify.app/" className="btn btn-primary" taget='_blank'>Live Demo</a>
                     </div>
                 </article>
 
